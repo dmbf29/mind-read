1ea29ba8c99cc5a0c44074a180e60491c44a7315
Douglas Berkley <douglasmberkley@gmail.com>
2023-06-06 16:03:10 +0900

    changed color and bg color

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,17 @@
-// This is where it all goes :)
+const button = document.getElementById("button");
+if (button) {
+  button.addEventListener("click", (event) => {
+    const number = document.getElementById("number").value;
+    const typed = new Typed("#typed", {
+      strings: [
+        "Analyzing brainwaves...",
+        "Scanning memories...",
+        "Decoding thoughts...",
+      ],
+      typeSpeed: 50,
+      onComplete: (self) => {
+        self.el.innerText = `You are thinking of the number ${number}.`;
+      },
+    });
+  });
+}
